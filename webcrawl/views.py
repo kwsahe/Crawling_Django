d7137d8b3a897920597b006832b181fa054314c1
Kwon_SangHeon <sangheon0187@gmail.com>
2026-05-11 11:13:23 +0900

    gradio-chatbot & movie_qry_search

--- webcrawl/views.py
+++ webcrawl/views.py
@@ -20,11 +20,35 @@ def movie_all_search(request):
     data = json.loads(response.text)
     print(data)
 
+    sendData = {
+        "searchKeyword" : "",
+        "qryData" : data
+    }
+
     # all search로 이동
-    return render(request, "webcrawl/movie_search.html", data)
+    return render(request, "webcrawl/movie_search.html", sendData)
 
 def movie_qry_search(request):
     print("func movie_qry_search")
     qry = request.GET.get('qry', '')
     print('qry = ',qry)
+
+    # AI 프로그램, 데이터베이스 처리 되는 루틴
+    # url = f"http://www.omdbapi.com/?s=movie&y=2026&apikey={apiKey}&page=1"
+    url = f"http://www.omdbapi.com/?apikey={apiKey}&s={qry}"
+
+    response = requests.get(url)
+
+    # 딕셔너리 변경 response.text
+    data = json.loads(response.text)
+    print(data)
+
+    sendData = {
+        "searchKeyword" : qry,
+        "qryData" : data
+    }
+
+    # all search로 이동
+    return render(request, "webcrawl/movie_search.html", sendData)
+
     return HttpResponse("func movie_qry_search")
